b81cc84f6cd2132863f70a5c5251f94cbbc88a5f
Jamie Jamison <jamison@library.ucla.edu>
2018-03-26 14:23:28 -0700

    initial commit

---
+++ Example.java
@@ -0,0 +1,11 @@
+/*	
+	This is a simple Java program.
+	file: Example.java
+*/
+
+class Example {	
+	// a java program gegins with a call to main()
+	public static void main(String args[]) {
+		System.out.println("Java drives the web!");
+	}
+}
---
+++ HelloWorldApp.java
@@ -0,0 +1,10 @@
+/*  
+    The HelloWorldApp class implements an application that
+    simply prints the ubiquitious hello-world to standard output
+*/
+ 
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!") ;  // display string
+    }
+} 
